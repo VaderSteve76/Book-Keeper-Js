a8b24df6858749c7e1350114ad59f01d885adf3a
Steve <alverson.s@yahoo.com>
2020-11-19 17:58:49 -0500

    Submit bookmarks working

--- script.js
+++ script.js
@@ -13,7 +13,36 @@ function showModal() {
   websiteNameEl.focus();
 }
 
+// Validate form
+function validate(nameValue, urlValue) {
+  const exp = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+  const regex = new RegExp(exp);
+  if(!nameValue || !urlValue) {
+    alert('Please submit values for both.');
+    return false;
+  }
+  if(!urlValue.match(regex)) {
+    alert('Please provide a valid web address');
+    return false;
+  }
+  return true;
+}
+
+// Submit data
+function storeBookmark(e) {
+  e.preventDefault();
+  const nameValue = websiteNameEl.value;
+  let urlValue = websiteUrlEl.value;
+  if(!urlValue.includes('http://', 'https://')) {
+    urlValue = `https://${urlValue}`;
+  }
+  if(!validate(nameValue, urlValue)) {
+    return false;
+  }
+}
+
 // Event listeners
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+bookmarkForm.addEventListener('submit', storeBookmark);
